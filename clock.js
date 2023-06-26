491e1d40cbba4bc514b5e3209b502935319ebc64
R.Sathish kumar <115446419+Sathiskumarssf@users.noreply.github.com>
2023-06-26 22:49:19 +0530

    Add files via upload
    this is the amazing clock poject to dispaly the current time and date.

---
+++ clock.js
@@ -0,0 +1,18 @@
+document.getElementById("timeset").onclick=function (){
+    setInterval(add,3000);
+     function add(){
+    const a =new Date();
+    console.log(a);
+    let b =a.getDate();
+    let c =a.getMonth()+1;
+    let d =a.getFullYear();
+    let f =a.getHours();
+    let g =a.getMinutes();
+    let h =a.getSeconds();
+    document.getElementById("clock").innerHTML= "today date is  :"+d+"/"+c+"/"+b+" =>"+f+":"+g+":"+h;
+    
+    }
+        
+    
+      
+    }
